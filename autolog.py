6c4406d48d82c8888076084983f0faed846cc57f
Henry Scheible <henry.scheible@gmail.com>
2021-03-13 23:41:53 -0600

    create 1st iteration of metalog

--- autolog.py
+++ autolog.py
@@ -1,10 +1,11 @@
 import json
+import time
 from networktables import NetworkTables
 
 json_path = input("Enter the JSON file path: ")
-json_file = open(json_path,"r")
-json_string = json_file.read().replace('\n', '')
-trial_object = json.loads(json_string)
+
+# json_string = json_file.read().replace('\n', '')
+# trial_object = json.loads(json_string)
 
 ip = input("Robot IP Address: ")
 
@@ -21,12 +22,22 @@ def connectionListener(connected, info):
 
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
-sd = NetworkTables.getTable("SmartDashboard")
+sd = NetworkTables.getTable("metaLog")
 sd.addEntryListener(valueChanged)
 
+results_object = []
+
 while True:
-    time.sleep(1)
-
-maxAcceleration
-
-"autolog" "maxAcceleration"
+    input("Press Enter to Read Log Data")
+    log_data = {}
+    log_data["timestamp"] = sd.getString("timeStamp", "no data")
+    log_data["elapsedAutoTime"] = sd.getNumber("elapsedAutoTime", 0)
+    log_data["successful"] = input("successful: (y/n) ") == "y"
+    log_data["markers_hit"] = input("Markers hit: ")
+    log_data["markers_hit"] = input("Markers hit: ")
+    log_data["markers_hit"] = input("Markers hit: ")
+    print("Log Data: ", log_data)
+    if input("Save: (y/n) ") == "y":
+        results_object.append(log_data)
+    with open(json_path,"w") as json_file:
+        json.dump(results_object, json_file)
